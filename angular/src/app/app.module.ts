47460a095ffa29d34f2443daed399595850fdc3d
RhodaMabs <mab21004@byui.edu>
2023-01-13 17:10:19 +0200

    Updates

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { WarningAlert } from './warning-alert/warning-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlert,
+    SuccessAlertComponent
   ],
   imports: [
-    BrowserModule
+  BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
