e5be9db7cfa9dee33314b683a197834c2ebcb5c2
RhodaMabs <mab21004@byui.edu>
2023-01-27 00:31:42 +0200

    Added new Components

--- cms/src/app/contacts/contact-list/contact-list.component.ts
+++ cms/src/app/contacts/contact-list/contact-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Contact } from '../contact.model'
 
 
@@ -8,12 +8,16 @@ import { Contact } from '../contact.model'
   styleUrls: ['./contact-list.component.css']
 })
 export class ContactListComponent {
-    contact: Contact[] = [
+  @Output() selectedContactEvent = new EventEmitter<Contact>();
+
+  @Input() contact: Contact[] = [
       new Contact("1", "R. Kent Jackson",
       "jacksonk@byui.edu","208-496-3771","../../assets/images/jacksonk.jpg","null" ),
       new Contact ("1","Rex Barzee",
       "barzeer@byui.edu","208-496-3768", "../../assets/images/barzeer.jpg","null")
+    ];
 
-
-    ]
+    onSelected(contact : Contact) {
+      this.selectedContactEvent.emit(contact);
+    }
 }
