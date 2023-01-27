65beb7cbf6ce390cea118a20c09260b0dff0a8c0
RhodaMabs <mab21004@byui.edu>
2023-01-27 17:10:09 +0200

    Updated

--- cms/src/app/app.module.ts
+++ cms/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { DocumentsComponent } from './documents/documents.component';
 import { DocumentListComponent } from './documents/document-list/document-list.component';
 import { DocumentDetailComponent } from './documents/document-detail/document-detail.component';
 import { DocumentItemComponent } from './documents/document-list/document-item/document-item.component';
+import { MessageItemComponent } from './messages/message-item/message-item.component';
+import { MessageEditComponent } from './messages/message-edit/message-edit.component';
+import { MessageListComponent } from './messages/message-list/message-list.component';
 
 
 @NgModule({
@@ -25,6 +28,9 @@ import { DocumentItemComponent } from './documents/document-list/document-item/d
     DocumentListComponent,
     DocumentDetailComponent,
     DocumentItemComponent,
+    MessageItemComponent,
+    MessageEditComponent,
+    MessageListComponent,
 
   ],
   imports: [
