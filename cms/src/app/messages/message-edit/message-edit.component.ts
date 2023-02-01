cb90eb59e121283d5ccaf002ac937c86ca57f0a8
RhodaMabs <mab21004@byui.edu>
2023-02-01 11:00:54 +0200

    Updates

--- cms/src/app/messages/message-edit/message-edit.component.ts
+++ cms/src/app/messages/message-edit/message-edit.component.ts
@@ -9,14 +9,14 @@ import { Message } from '../message.model';
 export class MessageEditComponent {
   @ViewChild('subject') subjectRef: ElementRef;
   @ViewChild('msgText') msgTextRef: ElementRef;
-  @Output() addMessageEvent = new EventEmitter<Message>(); 
+  @Output() addMessageEvent = new EventEmitter<Message>();
   currentSender = 'Rhoda Mabundu';
 
   onSendMessage() {
     const subject = this.subjectRef.nativeElement.value;
     const msgText = this.msgTextRef.nativeElement.value;
     const newMessage = new Message('1',subject, msgText, this.currentSender);
-    this.addMessageEvent .emit(newMessage);
+    this.addMessageEvent.emit(newMessage);
 
   }
   onClear() {
